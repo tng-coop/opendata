6651382a14a2519bcbf79c536bbb7b21dd0ace8c
Yasuaki Kudo <yasu@tng.coop>
2024-03-16 22:30:01 +0900

    a

--- index.php
+++ index.php
@@ -10,9 +10,8 @@ session_start();
 $scriptPathDir = dirname($_SERVER['PHP_SELF']);
 
 $uri = $_SERVER['REQUEST_URI'];
-
 // Adjust the regular expression for matching the UUID, making it relative to the script's directory
-if (preg_match('#^'.preg_quote($scriptPathDir).'/uuid/([a-f0-9\-]+)$#', $uri, $matches)) {
+if (preg_match('/\/uuid\/([a-f0-9\-]+)$/', $uri, $matches)) {
     // Display the UUID from the URL
     $uuidFromUrl = $matches[1];
     echo "UUID from URL: " . htmlspecialchars($uuidFromUrl) . "<br>";
@@ -23,7 +22,7 @@ if (preg_match('#^'.preg_quote($scriptPathDir).'/uuid/([a-f0-9\-]+)$#', $uri, $m
             $uuid4 = Uuid::uuid4();
             $_SESSION['generated_uuid'] = $uuid4->toString();
             // Redirect to a URL relative to the script's directory
-            header('Location: ' . $scriptPathDir . '/uuid/' . $uuid4->toString());
+            header('Location: ' . $scriptPathDir . 'uuid/' . $uuid4->toString());
             exit;
         } catch (Exception $e) {
             echo "Error: " . $e->getMessage();
