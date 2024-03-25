749fa5e96c0b1114abfb283b2eaa3061d368e864
Yasuaki Kudo <yasu@tng.coop>
2024-03-25 12:47:01 +0900

    a

--- UUIDGenerator.php
+++ UUIDGenerator.php
@@ -5,7 +5,9 @@ function generateAndRedirect($scriptPathDir) {
     try {
         $uuid4 = Uuid::uuid4();
         $_SESSION['generated_uuid'] = $uuid4->toString();
-        header('Location: ' . $scriptPathDir . './uuid/' . $uuid4->toString());
+       // Ensure there's no double slash if scriptPathDir is root ('/')
+       $redirectPath = rtrim($scriptPathDir, '/') . '/uuid/' . $uuid4->toString();
+       header('Location: ' . $redirectPath);
         exit;
     } catch (Exception $e) {
         echo "Error: " . $e->getMessage();
