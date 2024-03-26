9022990e6f27fc5bc23fcc1f971f6d485450f794
Yasuaki Kudo <yasu@yasuaki.com>
2024-03-26 22:53:53 +0900

    a

--- deploy-mayfirst.sh
+++ deploy-mayfirst.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPTDIR
+bash mayfirst-composer-install.sh
 opendata_dir=/home/members/yasuaki/sites/tng.coop/web/prod/opendata
 rm -rf $opendata_dir
 mkdir -p $opendata_dir
 
 rsync -av app/ $opendata_dir 
-cp  app $opendata_dir
 cp app-yasu.json $opendata_dir/app.json
-cp .htaccess $oppendata_dir 
+cp .htaccess $opendata_dir 
