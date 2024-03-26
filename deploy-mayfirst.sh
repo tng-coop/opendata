8c405ad3dab8f43b7462816290892dafdf051601
Yasuaki Kudo <yasu@yasuaki.com>
2024-03-26 23:40:42 +0900

    a

--- deploy-mayfirst.sh
+++ deploy-mayfirst.sh
@@ -2,11 +2,17 @@
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPTDIR
 bash mayfirst-composer-install.sh
-prod=/home/members/yasuaki/sites/tng.coop/web/prod/opendata
+cd $SCRIPTDIR/app
 #if exists, remove opendata symlink
-if [ -L $prod ]; then
-  rm $prod
+if [ -L app.json ]; then
+  rm app.json 
 fi
 #symlink app to prod
-ln -s $SCRIPTDIR/app $prod
+ln -s ../app-mayfirst.json app.json
+cd $SCRIPTDIR/../prod
+#if exists, remove opendata symlink
+if [ -L opendata ]; then
+  rm opendata 
+fi
+ln -s ../opendata/app opendata
 
