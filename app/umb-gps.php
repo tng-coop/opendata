f609f5110e3ecbe5da40275ab029f4c2ef73c462
Yasuaki Kudo <yasu@tng.coop>
2024-06-12 11:47:29 +0900

    a

--- app/umb-gps.php
+++ app/umb-gps.php
@@ -1,1 +1,13 @@
-hello
+<?php 
+// Display the UUID from the session
+echo 'UUID: ' . htmlspecialchars($_SESSION['umb-uuid']) . '<br>';
+
+// Display all GET parameters
+if (!empty($_GET)) {
+    echo 'GET Parameters:<br>';
+    foreach ($_GET as $key => $value) {
+        echo htmlspecialchars($key) . ': ' . htmlspecialchars($value) . '<br>';
+    }
+} else {
+    echo 'No GET parameters passed.';
+}
