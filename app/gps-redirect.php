6b98781598a19a4aea3714bd56fd35b85676484b
Yasuaki Kudo <yasu@tng.coop>
2024-06-12 13:03:03 +0900

    a

--- app/gps-redirect.php
+++ app/gps-redirect.php
@@ -1,16 +1,4 @@
-<!-- gps-info.php -->
-<div id="gps-info" style="margin-top: 20px;">
-    <h2>GPS Information</h2>
-    <div id="gps-coordinates">
-        <h3>Current Location:</h3>
-        <p id="latitudeGPS">Latitude: </p>
-        <p id="longitudeGPS">Longitude: </p>
-    </div>
-    <div id="fetched-content">
-        <h3>Fetched Content:</h3>
-        <div id="contentBox"></div>
-    </div>
-</div>
+<div id="contentBox"></div>
 
 <script>
     const displayGPSInfo = () => {
@@ -23,28 +11,28 @@
     };
 
     const showPosition = async (position) => {
-    const { latitude, longitude } = position.coords;
-    document.getElementById('latitudeGPS').textContent = `Latitude: ${latitude}`;
-    document.getElementById('longitudeGPS').textContent = `Longitude: ${longitude}`;
+        const { latitude, longitude } = position.coords;
+        document.getElementById('latitudeGPS').textContent = `Latitude: ${latitude}`;
+        document.getElementById('longitudeGPS').textContent = `Longitude: ${longitude}`;
 
-    try {
-        // Retrieve the UUID from the PHP session variable
-        const baseURL = '<?php echo $appConfig->get('url.base') . $appConfig->get('url.root') . 'umb-gps.php'; ?>';
-        const urlWithParams = `${baseURL}?latitude=${latitude}&longitude=${longitude}`;
+        try {
+            // Retrieve the UUID from the PHP session variable
+            const baseURL = '<?php echo $appConfig->get('url.base') . $appConfig->get('url.root') . 'umb-gps.php'; ?>';
+            const urlWithParams = `${baseURL}?latitude=${latitude}&longitude=${longitude}`;
 
-        // Perform the fetch request with the updated URL
-        const response = await fetch(urlWithParams, {
-            method: 'GET',
-        });
-        const data = await response.text();
-        document.getElementById('contentBox').innerHTML = data;
-    } catch (error) {
-        console.error('Error fetching content:', error);
-        document.getElementById('contentBox').innerHTML = 'Error fetching content.';
-        // also show error
-        document.getElementById('contentBox').innerHTML += `<p>${error}</p>`;
-    }
-};
+            // Perform the fetch request with the updated URL
+            const response = await fetch(urlWithParams, {
+                method: 'GET',
+            });
+            const data = await response.text();
+            document.getElementById('contentBox').innerHTML = data;
+        } catch (error) {
+            console.error('Error fetching content:', error);
+            document.getElementById('contentBox').innerHTML = 'Error fetching content.';
+            // also show error
+            document.getElementById('contentBox').innerHTML += `<p>${error}</p>`;
+        }
+    };
 
 
     document.addEventListener('DOMContentLoaded', displayGPSInfo);
