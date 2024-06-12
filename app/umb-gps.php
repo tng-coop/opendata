95719fc071ace955d4bd2a6bfd85b23eec067965
Yasuaki Kudo <yasu@tng.coop>
2024-06-12 12:58:41 +0900

    a

--- app/umb-gps.php
+++ app/umb-gps.php
@@ -1,69 +1,10 @@
-<!DOCTYPE html>
-<html>
+<?php
+if (isset($_GET['longitude']) && isset($_GET['latitude'])) {
+    $longitude = $_GET['longitude'];
+    $latitude = $_GET['latitude'];
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Application's Title</title>
-    <?php
-    require_once('leaflet.php');
-    ?>
-</head>
-
-<body>
-<div id="mapid"></div>
-<style>
-    #mapid {
-        -webkit-user-select: none;
-        /* Safari */
-        -moz-user-select: none;
-        /* Firefox */
-        -ms-user-select: none;
-        /* Internet Explorer/Edge */
-        user-select: none;
-        /* Non-prefixed version, currently supported by Chrome, Opera, and Edge */
-    }
-</style>
-<script>
-    // Initialize the map and set its view to our chosen geographical coordinates and a zoom level
-    // Function to check if the user is on a smartphone
-    function isSmartphone() {
-        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    }
-
-    var mymap = L.map('mapid', {
-        center: [35.1509, 139.1237],
-        zoom: 13,
-        scrollWheelZoom: false, // Disable scroll wheel zoom by default
-        dragging: !isSmartphone() // Disable dragging only for smartphones
-    });
-
-    $latitude = "<?= $_GET['latitude'] ?>";
-    $longitude = "<?= $_GET['longitude'] ?>";
-    <?php
-    // Use the fetchLatestBBS function to get the data
-    // $coordinates = fetchValidGpsData();
-    // Loop through the coordinates to place markers
-    // Loop through the coordinates to place markers
-    // function processCoordinate($latitude, $longitude)
-    // {
-    //     $uuid = $_SESSION['umb-uuid'];
-    //     global $appConfig;
-    //     $url = $appConfig->get('url.base') . $appConfig->get('url.root') . 'uuid/' . $uuid;
-    //     $la = $latitude;
-    //     $lo = $longitude;
-    //     $encodedUrl = json_encode($url);
-    //     $name='current'
-    //     ?>
-    //     var marker = L.marker([<?= json_encode($la) ?>, <?= json_encode($lo) ?>]).addTo(mymap);
-    //     marker.bindPopup('<a href=<?= $encodedUrl ?> target="_blank"><?= $name ?></a>');
-    //     <?php
-    // }
-    // foreach ($coordinates as $coordinate) {
-    //     // processCoordinate($coordinate);
-    // }
-    require_once('map-script.js'); ?>
-</script>
-</body>
-
-</html>
+    echo "Longitude: " . htmlspecialchars($longitude) . "<br>";
+    echo "Latitude: " . htmlspecialchars($latitude);
+} else {
+    echo "Longitude and latitude parameters are missing.";
+}
