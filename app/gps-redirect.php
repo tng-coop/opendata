2d0da4850a26fa62dc6c89622fb38b468b824c44
Yasuaki Kudo <yasu@tng.coop>
2024-06-12 13:09:42 +0900

    a

--- app/gps-redirect.php
+++ app/gps-redirect.php
@@ -3,10 +3,7 @@
     <h2>GPS Information</h2>
     <div id="gps-coordinates">
     </div>
-    <div id="fetched-content">
-        <h3>Fetched Content:</h3>
-        <div id="contentBox"></div>
-    </div>
+    <div id="contentBox"></div>
 </div>
 
 <script>
@@ -20,25 +17,25 @@
     };
 
     const showPosition = async (position) => {
-    const { latitude, longitude } = position.coords;
-    try {
-        // Retrieve the UUID from the PHP session variable
-        const baseURL = '<?php echo $appConfig->get('url.base') . $appConfig->get('url.root') . 'umb-gps.php'; ?>';
-        const urlWithParams = `${baseURL}?latitude=${latitude}&longitude=${longitude}`;
+        const { latitude, longitude } = position.coords;
+        try {
+            // Retrieve the UUID from the PHP session variable
+            const baseURL = '<?php echo $appConfig->get('url.base') . $appConfig->get('url.root') . 'umb-gps.php'; ?>';
+            const urlWithParams = `${baseURL}?latitude=${latitude}&longitude=${longitude}`;
 
-        // Perform the fetch request with the updated URL
-        const response = await fetch(urlWithParams, {
-            method: 'GET',
-        });
-        const data = await response.text();
-        document.getElementById('contentBox').innerHTML = data;
-    } catch (error) {
-        console.error('Error fetching content:', error);
-        document.getElementById('contentBox').innerHTML = 'Error fetching content.';
-        // also show error
-        document.getElementById('contentBox').innerHTML += `<p>${error}</p>`;
-    }
-};
+            // Perform the fetch request with the updated URL
+            const response = await fetch(urlWithParams, {
+                method: 'GET',
+            });
+            const data = await response.text();
+            document.getElementById('contentBox').innerHTML = data;
+        } catch (error) {
+            console.error('Error fetching content:', error);
+            document.getElementById('contentBox').innerHTML = 'Error fetching content.';
+            // also show error
+            document.getElementById('contentBox').innerHTML += `<p>${error}</p>`;
+        }
+    };
 
 
     document.addEventListener('DOMContentLoaded', displayGPSInfo);
