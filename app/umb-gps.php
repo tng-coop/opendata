a4256b42bf40166d2239b0c42937c7bb31c3a2ac
Yasuaki Kudo <yasu@tng.coop>
2024-06-12 13:42:27 +0900

    a

--- app/umb-gps.php
+++ app/umb-gps.php
@@ -1,7 +1,15 @@
+<script>
+    // JavaScript code to say "Hello"
+    document.addEventListener("DOMContentLoaded", function() {
+        console.log("Hello");
+        alert("Hello");
+    });
+</script>
 <?php
 if (isset($_GET['longitude']) && isset($_GET['latitude'])) {
     $longitude = $_GET['longitude'];
     $latitude = $_GET['latitude'];
+    
 
     echo "Longitude: " . htmlspecialchars($longitude) . "<br>";
     echo "Latitude: " . htmlspecialchars($latitude);
