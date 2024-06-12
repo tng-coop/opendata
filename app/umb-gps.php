2efe8d26a1878fb1855bc7ddb2910a42d07385bd
Yasuaki Kudo <yasu@tng.coop>
2024-06-12 13:57:52 +0900

    a

--- app/umb-gps.php
+++ app/umb-gps.php
@@ -1,15 +1,14 @@
-<script>
-    // JavaScript code to say "Hello"
-    document.addEventListener("DOMContentLoaded", function() {
-        console.log("Hello");
-        alert("Hello");
-    });
-</script>
 <?php
 if (isset($_GET['longitude']) && isset($_GET['latitude'])) {
     $longitude = $_GET['longitude'];
     $latitude = $_GET['latitude'];
-    
+    $uuid = $_SESSION['umb-uuid'];
+    appendOpDataWithUuid($uuid, [
+        'umb-gps' => [
+            'longitude' => $longitude,
+            'latitude' => $latitude
+        ]
+    ]);
 
     echo "Longitude: " . htmlspecialchars($longitude) . "<br>";
     echo "Latitude: " . htmlspecialchars($latitude);
