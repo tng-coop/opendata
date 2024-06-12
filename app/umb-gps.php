06096a41f486e9ea82c360bd2e7d654380b5ae3c
Yasuaki Kudo <yasu@tng.coop>
2024-06-12 12:33:52 +0900

    a

--- app/umb-gps.php
+++ app/umb-gps.php
@@ -1,13 +1,10 @@
-<?php 
-// Display the UUID from the session
-echo 'UUID: ' . htmlspecialchars($_SESSION['umb-uuid']) . '<br>';
+<?php
+if (isset($_GET['longitude']) && isset($_GET['latitude'])) {
+    $longitude = $_GET['longitude'];
+    $latitude = $_GET['latitude'];
 
-// Display all GET parameters
-if (!empty($_GET)) {
-    echo 'GET Parameters:<br>';
-    foreach ($_GET as $key => $value) {
-        echo htmlspecialchars($key) . ': ' . htmlspecialchars($value) . '<br>';
-    }
+    echo "Longitude: " . htmlspecialchars($longitude) . "<br>";
+    echo "Latitude: " . htmlspecialchars($latitude);
 } else {
-    echo 'No GET parameters passed.';
+    echo "Longitude and latitude parameters are missing.";
 }
