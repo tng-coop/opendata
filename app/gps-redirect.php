c423973e48450e63cbc1ee89301fb521c4911aa6
Yasuaki Kudo <yasu@tng.coop>
2024-06-12 13:05:39 +0900

    f

--- app/gps-redirect.php
+++ app/gps-redirect.php
@@ -1,5 +1,11 @@
 <!-- gps-info.php -->
 <div id="gps-info" style="margin-top: 20px;">
+    <h2>GPS Information</h2>
+    <div id="gps-coordinates">
+        <h3>Current Location:</h3>
+        <p id="latitudeGPS">Latitude: </p>
+        <p id="longitudeGPS">Longitude: </p>
+    </div>
     <div id="fetched-content">
         <h3>Fetched Content:</h3>
         <div id="contentBox"></div>
